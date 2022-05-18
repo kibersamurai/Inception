cdccb21832e8dd8d635840aab519159fbe311a64
Beth Provolone <bprovolo@un-b4.kzn.21-school.ru>
2022-05-18 21:31:41 +0300

    nginx conf

--- Inception/srcs/requirements/nginx/conf/nginx.sh
+++ Inception/srcs/requirements/nginx/conf/nginx.sh
@@ -4,7 +4,7 @@ openssl req -new -x509 -days 365 -key ca.key -out ca.crt -subj "/C=RU/ST=Kazan/L
 if [ -f /var/www/nginx.conf ]; then
 mv /var/www/nginx.conf /etc/nginx/sites-available/default
 fi
-echo "Starting nginx""
+echo "Starting nginx"
 chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
 exec "$@"
